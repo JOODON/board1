23010f665aa4c546d1a2072d14597cc47fa20b17
주동호 <100278301+JOODON@users.noreply.github.com>
2022-10-29 12:20:36 +0900

    로직 구현부터 다시해봅시당

--- src/main/java/com/study/board/repository/BoardRepository.java
+++ src/main/java/com/study/board/repository/BoardRepository.java
@@ -4,5 +4,5 @@ import com.study.board.entity.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BoardRepository extends JpaRepository<Board,Integer> {
-    //어떤 DTO를 참고할것인가를 먼저 선언하고 프라이머리키로 설저된 녀석을받아줌
+    //어떤 DTO를 참고할것인가를 먼저 선언하고 프라이머리키로 설저된 녀석을받아줌!
 }
